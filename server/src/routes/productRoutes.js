ebc1c91586dc15417f58622908814a7b54302c9f
realmadrid2 <tohran1@etf.unsa.ba>
2025-11-16 20:36:55 +0100

    feat: delete and findbypk routes

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,11 +1,12 @@
-// server/src/routes/productRoutes.js
 import express from 'express';
-import { getProducts, createProduct } from '../controllers/productController.js';
+import { getProducts, createProduct, getProductById, deleteProduct } from '../controllers/productController.js';
 import { protect, adminOnly } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.get('/:id', getProductById);
+router.delete('/:id', protect, adminOnly, deleteProduct);
 router.post('/', protect, adminOnly, createProduct);
 
 export default router;
